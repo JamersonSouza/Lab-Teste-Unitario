100b093b1278afdd1c744cd53ac971e36596716f
jamersonsouza <jamersonsouza@outlook.com>
2024-05-14 22:05:57 -0300

    lab para treinamento de testes unitários

---
+++ src/main/java/tech/jamersondev/labtestsunitarios/LabTestsUnitariosApplication.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.labtestsunitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTestsUnitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabTestsUnitariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/jamersondev/labtestsunitarios/LabTestsUnitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.labtestsunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabTestsUnitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
